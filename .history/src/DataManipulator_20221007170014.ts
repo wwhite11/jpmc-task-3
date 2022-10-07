7cb35397a09ed6719eb16493bc44010f64620ffc
wwhite11 <winstonrwhite@gmail.com>
2022-10-07 17:15:00 -0400

    Generated a live graph that displays data feed in a clear and visually appealing way for traders to monitor

---
+++ .history/src/DataManipulator_20221007170014.ts
@@ -0,0 +1,24 @@
+import { ServerRespond } from './DataStreamer';
+
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+  timestamp: Date,
+}
+
+
+export class DataManipulator {
+  static generateRow(serverResponds: ServerRespond[]) {
+    return serverResponds.map((el: any) => {
+      return {
+        stock: el.stock,
+        top_ask_price: el.top_ask && el.top_ask.price || 0,
+        timestamp: el.timestamp,
+      };
+    })
+  }
+}
